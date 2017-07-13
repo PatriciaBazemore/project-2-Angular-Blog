554e90f314b428eb88b2130980764bb08556f5e3
PatriciaBazemore <trishbazemore@gmail.com>
2017-07-12 20:09:52 -0400

    finished adding login and admin protections to api endpoints on the backend

--- server/controllers/users.ctrl.js
+++ server/controllers/users.ctrl.js
@@ -9,6 +9,29 @@ var router = express.Router();
 
 //actually api/users..used them up going through other files
 
+
+
+
+//actually api/users/login 
+router.post('/login', function(req, res, next) {    //not protected, dont need to be logged in to log in
+        passport.authenticate('local', function(err, user, info) {
+            if (err) { 
+                console.log(err); 
+                return res.sendStatus(500); 
+            } 
+            if (!user) { 
+                return res.status(401).send(info); 
+            }       
+            req.logIn(user, function(err) {
+                if (err) { 
+                    return res.sendStatus(500); 
+                } else { 
+                    return res.send(user); 
+                }        
+            });
+        })(req, res, next); //ife (immediately returning function and passing this in)
+    });
+
 router.all('*', auth.isLoggedIn);  //requires log in from this point down, all methods to all routes go through this, must be logged in to access below, 
                                     //you can do this individually like isAdmin
 
@@ -29,7 +52,7 @@ router.route('/')
             console.log(err);
             res.sendStatus(500);
         });
-    }).post(function(req, res) {
+    }).post(auth.isAdmin, function(req, res) {
         utils.encryptPassword(req.body.password)
         .then(function(hash) {
             return procedures.create(req.body.firstName, req.body.lastName, req.body.email, hash)  //must return to make chain wait before going to next step
@@ -40,29 +63,13 @@ router.route('/')
             sendStatus(500);
         })
         
-    }) 
+    }); 
     
 
 
 
 
-    .post('/login', function(req, res, next) {    //not protected, dont need to be logged in to log in
-        passport.authenticate('local', function(err, user, info) {
-            if (err) { 
-                console.log(err); 
-                return res.sendStatus(500); 
-            }if (!user) { 
-                return res.status(401).send(info); 
-            }       
-            req.logIn(user, function(err) {if (err) { 
-                return res.sendStatus(500); 
-            } else { 
-                return res.send(user); 
-            }        
-            });
-        })(req, res, next);
-    });
-
+    
 
 
 module.exports = router;
--- server/middleware/auth.mw.js
+++ server/middleware/auth.mw.js
@@ -1,7 +1,18 @@
 exports.isLoggedIn = function(req, res, next) {
-    if (req.user) {
-        next();
+    if (req.user) { //set by passport, links the session to logged in user 
+        next(); //let them through 
     } else {
-        res.sendStatus(401);
+        res.sendStatus(401); //we don't know you. unauthorized 
     }
 }
+
+exports.isAdmin = function(req, res, next) {
+    if (req.user.role === 'admin') { //needs to go through isLoggedIn before you call this because it's only called on users 
+        next(); //you're an admin, go on through
+    } else {
+        res.sendStatus(403); //I know you and you don't belog here. forbidden
+    }
+}
+
+//these two will the same for many projects
+//all mw has 3 params
